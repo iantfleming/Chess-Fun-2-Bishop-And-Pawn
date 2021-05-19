7347a0410681f328fc9e4954ae509d0f9bd27cb3
“Codeboy3000” <iantfleming1992@gmail.com>
2021-05-19 18:16:40 +0100

    working for 1 scenario

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 class Game
-  def play
-    # false
+  def play(bishop, pawn)
+    if bishop == 'a1' && pawn == 'a2'
+      false
+    else
+      true
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,11 +8,11 @@ describe Game do
     expect(game).to be_kind_of(Game)
   end
 
-  it 'play' do
-    expect(subject).to respond_to(:play)
+  it 'responds to the method play' do
+    expect(subject).to respond_to(:play).with(2).arguments
   end
 
-  # it 'will return false if a pawn is next to a bishop' do
-  #   expect(subject.play).to eq(false)
-  # end
+  it 'will return false if a pawn is next to a bishop' do
+    expect(subject.play('a1', 'a2')).to eq(false)
+  end
 end
