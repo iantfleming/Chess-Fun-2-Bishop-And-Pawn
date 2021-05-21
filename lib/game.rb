fd53ba4e2a307683a41067c68f5fd4761b230cd9
“Codeboy3000” <iantfleming1992@gmail.com>
2021-05-21 15:45:45 +0100

    convert string nums to real integers

--- lib/game.rb
+++ lib/game.rb
@@ -9,7 +9,29 @@ class Game
     b = bishop.split('')
     p = pawn.split('')
     piece_array = b + p
+    convert_to_integer(piece_array)
+    check_if_unique(piece_array)
+  end
+
+  def check_if_unique(piece_array)
     piece_array.uniq.size == piece_array.size
+  end
+
+  def convert_to_integer(piece_array)
+    piece_array.sort!
+
+    array2 = []
+
+    array2 << piece_array.slice(0)
+    array2 << piece_array.slice(1)
+
+    piece_array.delete_at(0)
+    piece_array.delete_at(0)
+
+    array2.map!(&:to_i)
+
+    piece_array << array2.slice(0)
+    piece_array << array2.slice(1)
   end
 end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -16,11 +16,60 @@ describe Game do
     expect(subject).to respond_to(:cooridinate_in_common).with(2).arguments
   end
 
+  it 'responds to the method convert_to_integer' do
+    expect(subject).to respond_to(:convert_to_integer).with(1).argument
+  end
+
+  # returning false
+
   it 'will return false if a pawn is next to a bishop' do
     expect(subject.play('a1', 'a2')).to eq(false)
   end
 
-  it 'will return false if a pawn is on the same row as a columm' do
+  it 'will return false if a pawn is on the same row as a bishop' do
     expect(subject.play('a2', 'a3')).to eq(false)
   end
+
+  it 'will return false if a pawn is on the same column as a bishop' do
+    expect(subject.play('a2', 'b2')).to eq(false)
+  end
+
+  it 'will return false if the bishop is on a8 and the pawn is on h7' do
+    expect(subject.play('a8', 'h7')).to eq(false)
+  end
+
+  # it is currently returning true unless the bishop and pawn share a coordinate.
+
+  # returning true
+
+  it 'if the bishop is on a and the pawn is on b, the pawns number must be 1 less than the bishops' do
+    expect(subject.play('a8', 'b7')).to eq(true)
+  end
+
+  it 'if the bishop is on a and the pawn is on c, the pawns number must be 2 less than the bishops' do
+    expect(subject.play('a8', 'c6')).to eq(true)
+  end
+
+  it 'if the bishop is on b and the pawn is on a, the pawns number must be 1 greater than the bishops' do
+    expect(subject.play('b7', 'a8')).to eq(true)
+  end
+
+  it 'if the bishop is on c and the pawn is on a, the pawns number must be 2 greater than the bishops' do
+    expect(subject.play('c6', 'a8')).to eq(true)
+  end
+
+  it 'the pawn is on h1 and the bishop is on a, it must be on a8 to return true' do
+    expect(subject.play('a8', 'h1')).to eq(true)
+  end
+
+  # convert_to_integer
+  it 'will take all string integers in array and convert to an actual integer' do
+    expect(subject.convert_to_integer(%w[a 1 a 2])).to eq(['a', 'a', 1, 2])
+  end
 end
+
+# my code is now separating coordinates into individual elements and converting string numbers to real integers
+
+# if the bishop is on a, for the program to return true
+# when the pawn is on b, its column must be minus 1
+# from whatever the a column is on
